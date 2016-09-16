5bf43defb3caad824d8ad52e83c66fa8c3df6bbf
Tony Poerio <tony@tonypoer.io>
2016-09-16 16:58:09 -0400

    Updated BFS. Now displaying required data.

--- hw01/bfs.py
+++ hw01/bfs.py
@@ -28,6 +28,12 @@ class Node:
         # store the current state
         self.current_state = state
 
+        # ensure we are assigning parent nodes correctly
+        #if parent_node is not None:
+        #    if state not in parent_node.successor_states:
+        #      suc_states = parent_node.successor_states
+        #      assert state in parent_node.successor_states
+
         # store the successor states
         self.successor_states = puzzle.getSuccessorStates(state)
 
@@ -44,6 +50,13 @@ class BFS:
 
        # careful here...
        self.current_node = None
+
+       # time/space data
+       self.solution_path = []
+       self.num_nodes = 0
+       self.frontier_max_size = 0
+       self.num_explored_states = 0
+
        return
 
    def bfs(self):
@@ -63,13 +76,28 @@ class BFS:
            next_state = self.frontier.pop()
 
            # this new state is now in our graph, so turn it into a node, and add it
-           next_node = Node(parent_node=deepcopy(self.current_node), state=next_state,
+           next_node = Node(parent_node=self.current_node, state=next_state,
                             puzzle=self.puzzle, total_cost=0)
+           self.graph.add(next_node)
+           self.current_node = next_node
 
            # check if we have a match, if so -- we've found the end state
            if self.puzzle.goalTest(next_state):
                # Need to return the path that got us here
-               print str(next_state)
+               print "BFS SOLUTION SEARCH PATH: "
+               final_node = next_node
+               path_list = [str(final_node.current_state)]
+
+               while final_node.parentNode is not None:
+                   path_list.append(str(final_node.parentNode.current_state))
+                   final_node = final_node.parentNode
+
+               path_list.reverse()
+               for elem in path_list:
+                    print "\t->" + str(elem)
+               print "TIME:   Number of Nodes Created="+str(self.num_nodes)
+               print "SPACE:  Frontier Max-Size="+str(self.frontier_max_size)
+               print "SPACE:  Number of States Explored="+str(self.num_explored_states)
                return True
 
            # add the node to the explored list, if it's not a match
@@ -77,9 +105,21 @@ class BFS:
 
            # and expand our frontier, so that it contains everything on the list of our new node
            for elem in next_node.successor_states:
-                self.frontier.appendleft(elem)
+               # but don't add anything that's already in frontier or explored...
+                if not elem in self.explored and \
+                   not elem in self.frontier:
+                    self.frontier.appendleft(elem)
 
-           # test if we have the correct goal state
+           # book-keeping
+           # update frontier size
+           if len(self.frontier) > self.frontier_max_size:
+               self.frontier_max_size = len(self.frontier)
+           # update explored size
+           if len(self.explored) > self.num_explored_states:
+               self.num_explored_states = len(self.explored)
+           # update graph size
+           self.num_nodes = len(self.graph)
+
 
        return
 
