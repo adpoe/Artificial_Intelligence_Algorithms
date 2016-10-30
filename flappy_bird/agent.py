9ffb5940278f7df195f74219505ea451f102a475
Tony Poerio <tony@tonypoer.io>
2016-10-29 22:59:21 -0400

    created ranom-acting agent that runs game 10x. just getting oriented with the code base and making sure i can send messages to the bird.

---
+++ flappy_bird/agent.py
@@ -0,0 +1,10 @@
+import flappybird
+
+def main():
+    bird = flappybird.Bird()
+    return
+
+if __name__ == '__main__':
+    # If this module had been imported, __name__ would be 'flappybird'.
+    # It was executed (e.g. by double-clicking the file), so call main.
+    main()
